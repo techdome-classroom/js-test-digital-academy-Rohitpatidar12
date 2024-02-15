1a9628582ea16cab7f608d780455c03c3f10e4c0
Rohitpatidar12 <rohitpatidar8765@gmail.com>
2024-02-15 16:27:47 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,8 +3,27 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
-};
+    const stack = [];
+    const brackets = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (char in brackets) {
+            stack.push(char);
+        } else {
+            const lastBracket = stack.pop();
+            if (brackets[lastBracket] !== char) {
+                return false; npm
+            }
+        }
+    }
+
+    return stack.length === 0; 
+}
 
 module.exports = { isValid };
 
